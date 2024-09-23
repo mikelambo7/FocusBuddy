6b8480e5fde6e5aecd9d2b581c0f3104486916a6
michaellambo <lambomichael7@gmail.com>
2024-09-22 19:29:32 -0500

    Implement sound alert that plays when user loses focus

--- src/components/FocusSession.js
+++ src/components/FocusSession.js
@@ -1,5 +1,6 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import WebcamFeed from './WebcamFeed';
+import pingSound from './assets/ping_alert.wav';
 import './HomePage.css';
 
 const FocusSession = ({ setSessionActive }) => {
@@ -11,12 +12,13 @@ const FocusSession = ({ setSessionActive }) => {
   const [sessionStats, setSessionStats] = useState(null);
   const [showNotification, setShowNotification] = useState(false); // Notification displayed for when a session is saved successfully
 
-
+  const alertSound = new Audio(pingSound);
 
   /* To process whether a face has been detected by the webcam */
   const handleFaceDetected = useCallback((isDetected) => { // useCallback ensures function is memoized and not recreated on every render
     if (!isDetected) {
       setNoFaceTime(prevTime => prevTime + 1); // Increment noFaceTime by 1 second
+      setTotalUnfocusedTime(prevTime => prevTime + 1) // Increment total unfocused time
     } else {
       setNoFaceTime(0); // Reset noFaceTime when face is detected
     }
@@ -25,7 +27,8 @@ const FocusSession = ({ setSessionActive }) => {
   useEffect(() => {
     // Check every second if no face is detected for more than 3 seconds
     const interval = setInterval(() => {
-      if (noFaceTime > 3) {
+      if (noFaceTime > 0) {
+        alertSound.play();
         setFocusLostCount(prevCount => prevCount + 1); // Increment focus lost count if face isn't detected for more than 3 seconds
         setNoFaceTime(0); // Reset noFaceTime after logging the loss of focus
       }
@@ -44,7 +47,7 @@ const FocusSession = ({ setSessionActive }) => {
       endTime: new Date(),
       attentionSpans: attentionData, // Array of attention metrics
       totalSessionTime: Math.floor((new Date() - startTime) / 1000), // in seconds
-      totalFocusedTime: Math.floor((new Date() - startTime) / 1000) - totalUnfocusedTime, // in seconds
+      totalTimeUnfocused: totalUnfocusedTime, // in seconds
       totalFocusLost: focusLostCount,
     };
 
@@ -79,6 +82,19 @@ const FocusSession = ({ setSessionActive }) => {
     }
   };
 
+  // Formats the times as minutes and seconds
+  const formatTime = (seconds) => {
+    const mins = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    return `${mins} min ${secs} sec`;
+  };
+
+  // Calculates the percentage of a session the user was unfocused
+  const calculateUnfocusedPercentage = (sessionTime, unfocusedTime) => {
+    if (sessionTime === 0) return 0; // To avoid division by zero
+    return ((unfocusedTime / sessionTime) * 100).toFixed(2);
+  };
+
   return (
     <div className="focus-session-container">
       <button className="session end" onClick={endSession}>
@@ -88,11 +104,6 @@ const FocusSession = ({ setSessionActive }) => {
       {/* <div className="webcam-feed">
         <WebcamFeed onFaceDetected={handleFaceDetected} />
       </div> */}
-      {/* {alert &&
-        <div className="alert-container">
-          <div className='alert-header'>Stay focused!</div>
-          <div className='alert'>Are you still there?</div>
-        </div>} */}
 
       {showNotification && (
         <div className="notification-bar">
@@ -104,9 +115,11 @@ const FocusSession = ({ setSessionActive }) => {
         <div className="session-stats-container">
           <div className="session-stats">
             <h2>Session Stats</h2>
-            <p>Total Session Time: 30 seconds</p>
-            <p>Time Focused: 20 seconds</p>
-            <p>Total Times Focus Lost: 6 times</p>
+            <p>Total Session Time: {formatTime(sessionStats.totalSessionTime)}</p>
+            <p>Time Focused: {formatTime(sessionStats.totalSessionTime - sessionStats.totalTimeUnfocused)}</p>
+            <p>Total Times Focus Lost: {formatTime(sessionStats.totalFocusLost)}</p>
+
+            <p>You were unfocused for a total of {calculateUnfocusedPercentage(sessionStats.totalSessionTime, sessionStats.totalTimeUnfocused)}% of your session</p>
             <button onClick={() => setSessionStats(null)}>Continue</button>
           </div>
         </div>
